b37b9edf7f3b4f6a32191d6376780161dc2ed2f2
yorvismel <112412149+yorvismel@users.noreply.github.com>
2023-06-29 22:54:18 -0500

    Haciendo modificaciones basicas para la practica de gitflow. YM

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 function suma() {
     return 1 + 2;
-}
+} 
+
+
+let person = {
+    first: "Juan",
+    last: "Perez"
+    };
+   let rep =  person.first.replace("Juan", "lola")
+    console.log(rep);
+
